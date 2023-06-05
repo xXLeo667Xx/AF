28ad58b3fb07ff80b44db03ab9c03d12168bd0a2
aluno <aluno@A2707>
2023-06-05 20:59:44 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Course
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double num;
+
+            Console.WriteLine("digite um numero intero psitivo:");
+            num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("os primeiros numeros " + num + " numeros pares são:");
+
+            for (int i = 2; i <= num * 2; i += 2)
+            {
+                Console.WriteLine(i.ToString(CultureInfo.InvariantCulture) + " ");
+            }
+
+        }   
+
+    }
+}
