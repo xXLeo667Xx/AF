8f13b7278a9fea3e604616dbad37671bd2888351
aluno <aluno@A2707>
2023-06-05 21:55:08 -0300

    update

--- Program.cs
+++ Program.cs
@@ -7,21 +7,56 @@ namespace Course
     {
         public static void Main(string[] args)
         {
-            double num, i = 1;
+            char resposta;
+
 
             do
             {
 
-                Console.WriteLine("digite um numero de 1 a 10 ");
-                num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                float num1, num2;
+                char op;
+
+                Console.WriteLine("entre com o operador: +, -, /, *: ");
+                op = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("entre com dois valores: ");
+                num1 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                num2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                switch (op)
+                {
+                    case '+':
+                        Console.Write(num1 + " + " + num2 + "=");
+                        float soma1 = num1 + num2;
+                        Console.WriteLine(soma1);
+                        break;
+
+                    case '-':
+                        Console.Write(num1 + "-" + num2 + "=");
+                        float soma2 = num1 - num2;
+                        Console.WriteLine(soma2);
+                        break;
+
+                    case '/':
+                        Console.WriteLine(num1 + "/" + num2 + "=");
+                        float soma3 = num1 / num2;
+                        Console.WriteLine(soma3);
+                        break;
+
+                    case '*':
+                        Console.WriteLine(num1 + "*" + num2 + "=");
+                        float soma4 = num1 * num2;
+                        Console.WriteLine(soma4);
+                        break;
+
+                    default:
+                        Console.WriteLine("operador errado, insira operador correto");
+                        break;
+                }
+                Console.WriteLine("deseja fazer a conta de novo(S/N): ");
+                resposta = char.Parse(Console.ReadLine());
             }
-            while (num < 1 || num > 10);
-               
-                    Console.WriteLine("voce digitou o numero " + num +".");
-
-                
-            
-        }   
-
+            while (resposta == 's' || resposta == 'S');
+        }
     }
 }
