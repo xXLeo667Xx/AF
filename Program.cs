b82339e8d2a9e8f37b93b21c101ad7b8a208337b
xXLeo667Xx <127798966+xXLeo667Xx@users.noreply.github.com>
2023-06-07 00:25:31 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,14 @@
-﻿using System;
+//colocando uma blibioteca
+using System;
 using System.Globalization;
-
+// nome do programa
 namespace Course
 {
-    public class Program
+    public class Program//classe programa
     {
        
        
-
+          //um programa para calculo de soma 
           public static int Soma(int a, int b)
             {
                 int resultado;
@@ -16,7 +17,7 @@ namespace Course
 
                 return resultado;   
             }
-           public static double Divi(double a, double b)
+           public static double Divi(double a, double b)//um programa para calculo de divizão
             {
                 double resulatdo;
 
@@ -25,7 +26,7 @@ namespace Course
                 return resulatdo;
             }
 
-           public static int Sulb(int a, int b)
+           public static int Sulb(int a, int b)//um programa para calculo de sulbitração
             {
                 int resultado;
 
@@ -34,7 +35,7 @@ namespace Course
                 return resultado;
             }
 
-            public static int Mult(int a, int b)
+            public static int Mult(int a, int b)//um programa para calculo de multiplicação
             {
                 int resultado;
 
@@ -43,23 +44,20 @@ namespace Course
                 return resultado;
             }
 
-        public static void Main(string[] args)
+        public static void Main(string[] args)// programa principal 
         {
+            //variaveis como valores ja atribuidos 
             int a, b;
             int soma = Soma(5, 7);
             int sulb = Sulb(5, 7);
             double divi = Divi(5, 3);
             int mult = Mult(5, 7);
-
+            //interface onde mostrara o calculo das contas 
             Console.WriteLine(soma.ToString(CultureInfo.InvariantCulture));
             Console.WriteLine(divi.ToString("F4",CultureInfo.InvariantCulture));
             Console.WriteLine(sulb.ToString(CultureInfo.InvariantCulture));
             Console.WriteLine(mult.ToString( CultureInfo.InvariantCulture));
 
         }
-
-
-
-
     }
 }
