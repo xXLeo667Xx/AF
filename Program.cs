585209d1bbfa281b27f4030b3f35495d0e462d33
xXLeo667Xx <127798966+xXLeo667Xx@users.noreply.github.com>
2023-06-07 00:41:49 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
-﻿using System;
+//colocando biblioteca
+using System;
 using System.Globalization;
-
+//nome do programa
 namespace Course
 {
-    public class Program
+    public class Program//classe programa
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args)//programa principal
         {
-            double num, i = 1;
-
+            double num, i = 1;//variaveis 
+             //interface de usuario aonde atribuira um valor a num 
             Console.WriteLine("digite um numero intero positivo:");
             num = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-
+              //codigo aonde repetirar o numero que eu colocar 
             while (i <= num)
             {
+                //interface aonde mostrara o numero
                 Console.WriteLine(i.ToString(CultureInfo.InvariantCulture) + " ");
-
+                
                 i++;
             }
         }   
